a7d8a85d5011ce054bf264e0763d0d838fc1cc2d
alexi-gamon <gamon.aguilar@outlook.es>
2022-10-21 19:37:09 -0500

    first commit

---
+++ holamundo/holamundo/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn holamundo.wsgi --log-file -
---
+++ holamundo/mundo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def homePageView(PageView):
+    return('hola mundo')
+
+# Create your views here.
